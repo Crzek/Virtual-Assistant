ab04f231cfac1dbb67dd9731d88889c3773bafa5
CruzWin <javier1996.ec@gmail.com>
2023-01-29 23:25:05 +0100

    voz

--- test/proof.py
+++ test/proof.py
@@ -3,12 +3,14 @@ import pyttsx3
 
 engine = pyttsx3.init('sapi5')
 
-engine.setProperty("rate",180)          #velocidad de la voz
-# voz = engine.getProperty("voices")
-# for v in voz:
-#     print(f"Voz: {v.name}")
+#engine.setProperty("rate",180)          #velocidad de la voz
+voz = engine.getProperty("voices")
+for v in voz:
+  print(f"Voz: {v.name}")
 
+"""
 engine.say('bienvenido, buenas soy jarvis, tu quien eres?')
 engine.runAndWait()
 voz = pyttsx3.voice.Voice
 print(voz)
+"""
